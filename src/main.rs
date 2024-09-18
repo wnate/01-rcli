2d040a16b50786babc8743eaa12caaaf46e4fa25
nate <itgitor@gmail.com>
2024-09-18 15:50:10 +0800

    feature: add csv cli support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+// rcli csv -i input.csv -o output.json --header -d ','
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
